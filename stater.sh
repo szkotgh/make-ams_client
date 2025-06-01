b0a4991cb363dc69e21ac40c6db5ef6901f36cb1
szkotgh <me@szk.kr>
2025-06-02 08:39:14 +0900

    update: service root check disabled

--- stater.sh
+++ stater.sh
@@ -7,12 +7,12 @@ DIR="$( cd "$( dirname "$0" )" && pwd -P )"
 echo $SERVICE_NAME Path=\'$DIR\'
 cd "$DIR"
 
-# Check if the script is running as root
-if [ "$(id -u)" -ne 0 ]; then
-    echo "$SERVICE_NAME stater must be run as root."
-    echo "Try 'sudo bash $0'"
-    exit 1
-fi
+# # Check if the script is running as root
+# if [ "$(id -u)" -ne 0 ]; then
+#     echo "$SERVICE_NAME stater must be run as root."
+#     echo "Try 'sudo bash $0'"
+#     exit 1
+# fi
 
 # if git is installed, pull the latest changes
 if command -v git &> /dev/null
