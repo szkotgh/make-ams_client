9e17c8f9284f7b5872edd9b2333add8056409989
szkotgh <me@szk.kr>
2025-06-02 16:39:02 +0900

    update: update

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import sys
 from page_manager import App
+import tkinter
 
 if __name__ == "__main__":
     try:
         app = App()
         app.mainloop()
+    except tkinter.TclError as e:
+        print("Tkinter GUI Fail:", e)
+        sys.exit(1)
     except Exception:
-        sys.exit(0)
+        sys.exit(1)
