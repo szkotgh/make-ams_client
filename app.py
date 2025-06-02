a31cab4e639415313c0d1f94e1595ab8cc4f14cb
szkotgh <me@szk.kr>
2025-06-02 11:06:07 +0900

    update: update

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import sys
 from page_manager import App
-import config
 
 if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+    try:
+        app = App()
+        app.mainloop()
+    except Exception:
+        sys.exit(0)
