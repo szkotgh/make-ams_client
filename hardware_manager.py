860831b671afa7af83229a569c926b1e8e2fd701
szkotgh <me@szk.kr>
2025-06-04 19:28:24 +0900

    update: code update

--- hardware_manager.py
+++ hardware_manager.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-import time
+import threading
 
 class HardwareManager():
     def __init__(self, RELAY_PIN=17):
@@ -12,10 +12,11 @@ class HardwareManager():
         GPIO.output(self.RELAY_PIN, GPIO.HIGH if state else GPIO.LOW)
         pass
     
-    def open_door(self):
+    def open_door(self, duration=1):
         self.set_door(True)
-        time.sleep(0.1)
-        self.set_door(False)
+        def close_door():
+            self.set_door(False)
+        threading.Timer(duration, close_door).start()
     
     def hardware_close(self):
         GPIO.cleanup()
