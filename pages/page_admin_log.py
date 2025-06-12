b7a5103c29bac9fc378a2a2856b611a75e05f0d8
szkotgh <me@szk.kr>
2025-06-12 23:04:25 +0900

    update: limit update

--- pages/page_admin_log.py
+++ pages/page_admin_log.py
@@ -12,7 +12,7 @@ class PageAdminLog(tk.Frame):
         title_frame.pack(pady=20)
         
         tk.Label(title_frame, text="로그열람", font=(config.DEFAULT_FONT, 28, "bold"), fg="black").pack()
-        tk.Label(title_frame, text="내부 장치 로그입니다. 최근 100개의 로그만 표시됩니다.", font=(config.DEFAULT_FONT, 14), fg="black").pack()
+        tk.Label(title_frame, text="내부 장치 로그입니다. 최근 500개의 로그만 표시됩니다.", font=(config.DEFAULT_FONT, 14), fg="black").pack()
         
         self.text_area = tk.Text(self, font=(config.DEFAULT_FONT, 12), width=58, height=10)
         self.text_area.pack(expand=True, fill="both", padx=20)
@@ -22,7 +22,7 @@ class PageAdminLog(tk.Frame):
 
     def refresh_log(self):
         self.text_area.delete("1.0", tk.END)
-        logs = log_manager.service.get_logs(limit=100)
+        logs = log_manager.service.get_logs(limit=500)
         for log in logs:
             time, method, action, details = log[1], log[2], log[3], log[4]
             self.text_area.insert(tk.END, f"{time} | {method} | {action} | {details}\n")
